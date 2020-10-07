a4c8b901af225b0b52d61a3a0004fac968f0a6ee
wnghks0531 <wnghks0531>
2020-10-07 19:06:32 +0900

    Updates

---
+++ precache-manifest.70dcef58430e944256dc94a81bd25b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052d497804425d004e4f1d65c384d717",
+    "url": "/react-Youtube/index.html"
+  },
+  {
+    "revision": "8a042cc767e902c95ce8",
+    "url": "/react-Youtube/static/css/main.d43632fe.chunk.css"
+  },
+  {
+    "revision": "72767f7cca2627ec4870",
+    "url": "/react-Youtube/static/js/2.4a3b0c0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Youtube/static/js/2.4a3b0c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a042cc767e902c95ce8",
+    "url": "/react-Youtube/static/js/main.6f01988a.chunk.js"
+  },
+  {
+    "revision": "fd488097f28005d3a75f",
+    "url": "/react-Youtube/static/js/runtime-main.bd188829.js"
+  }
+]);
